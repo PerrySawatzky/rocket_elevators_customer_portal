ef2d25fa97daea2c6ac2ea33d951a2d2a8c9c96a
PerrySawatzky <sawatzky.perry@gmail.com>
2021-07-26 13:45:44 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rocket_elevators_customer_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
