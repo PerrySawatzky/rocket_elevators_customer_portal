92bda79a9bb6299fb9f7f7c7e006ee7e1e45b6fb
PerrySawatzky <sawatzky.perry@gmail.com>
2021-07-29 12:36:59 -0400

    added intervention form user verification

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,15 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-async function getUsers(__RequestVerificationToken) {
-    const url = "https://whispering-tundra-91467.herokuapp.com/api/users/nicolas.genest@codeboxx.biz/";
-fetch(url)
- .then(
-     response => console.log(response)// .json(), etc.
-     // same as function(response) {return response.text();}
- );
+
    
      
 
- } 
+ 
  
